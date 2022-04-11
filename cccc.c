b3e0eb435736b4eb9cf8c9d5360591b9a7a21b53
Lin Yang <geekya215@gmail.com>
2022-04-11 21:16:54 +0800

    add vm instructions

---
+++ cccc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+enum {
+  IMM ,LEA ,LI  ,SI  ,LC  ,SC  ,PUSH,
+  CALL,JMP ,JZ  ,JNZ ,ENT ,ADJ ,LEV ,
+  ADD ,SUB ,MUL ,DIV ,MOD ,SHL ,SHR ,AND ,OR  ,XOR ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,
+  OPEN,READ,CLOS,PRTF,MALC,FREE,MSET,MCMP,EXIT
+};
+
+int main (int argc, char *argv[]) {
+  return 0;
+}
