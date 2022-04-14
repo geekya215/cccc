426e3da689aee97ca98362ffef9f962bc198fc70
Lin Yang <geekya215@gmail.com>
2022-04-15 01:04:23 +0800

    load source code to buffer

--- cccc.c
+++ cccc.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int token,
+    poolsize,
+    line;
+char *src;
 
 enum {
   IMM ,LEA ,LI  ,SI  ,LC  ,SC  ,PUSH,
@@ -12,12 +20,18 @@ enum {
 };
 
 void next() {
+  token = *src++;
 }
 
 void expression(int level) {
 }
 
 void program() {
+  next();
+  while (token > 0) {
+    printf("read token: %c\n", token);
+    next();
+  }
 }
 
 int eval() {
@@ -25,5 +39,34 @@ int eval() {
 }
 
 int main(int argc, char *argv[]) {
-  return 0;
+  int i, fd;
+
+  // initial 
+  poolsize = 256 * 1024;
+  line = 1;
+  argc--, argv++;
+
+  // create fd
+  if ((fd = open(*argv, 0)) < 0) {
+    printf("could not open %s\n", *argv);
+    return -1;
+  }
+
+  // malloc memory
+  if (!(src = malloc(poolsize))) {
+    printf("could not malloc %d bytes memory\n", poolsize);
+    return -1;
+  }
+
+  // read source code to buffer
+  if ((i = read(fd, src, poolsize - 1)) <= 0) {
+    printf("load %d bytes to memory\n", i);
+    return -1;
+  }
+
+  src[i] = 0;
+  close(fd);
+
+  program();
+  return eval();
 }
